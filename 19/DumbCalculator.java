788702b44ebecb12ab41bc9703ecb9635096c70e
elo18 <36411150+elo18@users.noreply.github.com>
2018-02-21 09:44:38 -0500

    Add files via upload

---
+++ 19/DumbCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DumbCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("What is your first number? ");
+        double firstNum = input.nextDouble();
+        
+        System.out.print("What is your second number? ");
+        double secondNum = input.nextDouble();
+        
+        System.out.print("What is your third number? ");
+        double thirdNum = input.nextDouble();
+        
+        System.out.println("( " + firstNum + " + " + secondNum + " + " + thirdNum + " ) / 2 is... " + (firstNum + secondNum + thirdNum) / 2);
+        }
+   }
